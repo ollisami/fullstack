3cb22c97d9f9f126130f97386339de19e16f2448
Ollila <sami-pekka.ollila@cgi.com>
2019-01-28 14:34:15 +0200

    osa2_20_puhelinluettelo_virheviestit

--- osa2_puhelinluettelo/src/App.js
+++ osa2_puhelinluettelo/src/App.js
@@ -3,6 +3,7 @@ import personService from './services/personService'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
+import Notification from './components/Notification'
 
 const App = () => {
 
@@ -10,14 +11,36 @@ const App = () => {
   const [ newName, setNewName ] = useState('')
   const [ newPhone, setNewPhone ] = useState('')
   const [ filter, setFilter ] = useState('')
+  const [message, setMessage] = useState({
+    message: '',
+    isError: true
+  })
 
-  useEffect(() => {
+  useEffect(() => updatePersonList(), [])
+
+  const updatePersonList = () => {
     personService
       .getAll()
       .then(initialPersons => {
         setPersons(initialPersons)
       })
-  }, [])
+      .catch(error => {
+        setTimedMessage('Haku epäonnistui', true)
+      })
+  }
+
+  const setTimedMessage = (newMessage, isError) => {
+    setMessage({
+      message: newMessage,
+      isError: isError
+    })
+    setTimeout(() => {
+      setMessage({
+        message: '',
+        isError: false
+      })
+    }, 5000)
+  }
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -32,12 +55,21 @@ const App = () => {
         .create(personObject)
         .then(returnedPerson => {
           setPersons(persons.concat(returnedPerson))
+          setTimedMessage(`Henkilön ${returnedPerson.name} lisäys onnistui`, false)
+      })
+      .catch(error => {
+        setTimedMessage('Henkilön lisäys epäonnistui', true)
       })
     } else if (window.confirm(`${newName} on jo luettelossa, korvataanko vanha numero uudella?`)) {
         personService
           .update(current.id, personObject)
           .then(returnedPerson => {
             setPersons(persons.map( person => person.id !== current.id ? person : returnedPerson))
+            setTimedMessage(`Henkilön ${returnedPerson.name}  päivitys onnistui`, false)
+      })
+      .catch(error => {
+        setTimedMessage(`Henkilö ${newName} oli jo poistettu`, true)
+        updatePersonList()
       })
     }
     setNewName('')
@@ -65,6 +97,10 @@ const App = () => {
         .remove(id)
         .then(initialPersons => {
           setPersons(initialPersons)
+          setTimedMessage(`Henkilön ${p.name}  poisto onnistui`, false)
+        })
+        .catch(error => {
+          setTimedMessage('Henkilön poisto epäonnistui', true)
         })
     }
   }
@@ -72,6 +108,7 @@ const App = () => {
   return (
     <div>
       <h2>Puhelinluettelo</h2>
+      <Notification message={message} />
       <Filter filter={filter} filterChange={handleFilterChange} />
 
       <h3> lisää uusi </h3>
